3107b305c24941fcaf3c389ee2b34807cdac8ab6
liufangpu <424395537@qq.com>
2016-12-15 22:26:47 +0800

    331

---
+++ src/site/jjilikeyou/www/dao/UserDao.java
@@ -0,0 +1,11 @@
+package site.jjilikeyou.www.dao;
+
+import org.apache.ibatis.annotations.Param;
+
+import site.jjilikeyou.www.pojo.User;
+
+public interface UserDao {
+
+	User checkName(@Param("username")String username);
+
+}
---
+++ src/site/jjilikeyou/www/service/UserService.java
@@ -0,0 +1,9 @@
+package site.jjilikeyou.www.service;
+
+import site.jjilikeyou.www.pojo.User;
+
+public interface UserService {
+
+	User checkName(String username);
+  
+}
